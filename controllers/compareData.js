c7c9adb39c0ef6fe9b934cb9ba73a8fdd2430a41
Luke Gartland <lukegartland@gmail.com>
2021-08-18 11:06:08 -1000

    search for missed and added data

--- controllers/compareData.js
+++ controllers/compareData.js
@@ -34,9 +34,35 @@ const compareData = (req, res) => {
             findCorruptedData(newData.rows, oldData.rows)
             const duplicates = findDuplicates(newData.rows)
 
-            res.status(200).send({duplicates: duplicates.length, corrupted: corruptedObjs.length, corruptedObjs  })
+            const missedAndNew = (newData, oldData) => {
+                const oldIdArr = oldData.map(d => d.id)
+                const newIdArr = newData.map(d => d.id)
+                let missedAndAdded = []
+                let missedData = []
+                let addedData = []
+                let holdId = []
+                oldData.forEach(document => {
+                    // if new data doesn't include this old document it was missed
+                    if (!newIdArr.includes(document.id)) {
+                        missedData.push(document)
+                        missedAndAdded.push(document)
+                        // not needed for this program added for code reusability
+                        holdId.push(document.id)
+                    }
+                })
 
+                newData.forEach(document => {
+                    // if old data does not include this new document it was added and it hasn't already been added to missed and added list
+                    if (!oldIdArr.includes(document.id) && !holdId.includes(document.id)) {
+                        addedData.push(document)
+                        holdId.push(document.id)
+                    }
+                })
+                return {missedAndAdded, missedData, addedData}
+            }
+           const dataMissedAndAdded = missedAndNew(newData.rows, oldData.rows)
 
+            res.status(200).send({total: dataMissedAndAdded.missedAndAdded.length, missed: dataMissedAndAdded.missedData.length, added: dataMissedAndAdded.addedData.length, duplicates: duplicates.length, corrupted: corruptedObjs.length, dataMissedAndAdded, corruptedObjs })
         }
             })
         } 
