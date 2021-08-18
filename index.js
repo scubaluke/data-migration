0612f8806c8ed16c157b1cc0f5defbff24c2b1ce
Luke Gartland <lukegartland@gmail.com>
2021-08-18 07:26:44 -1000

    filter for corrupted data

--- index.js
+++ index.js
@@ -53,10 +53,50 @@ app.get('/new', (req, res) => {
         pool.end()
     })
 })
-// todo - find:
-// 1. Missing data 
-// 2. corrupted data 
-// 3. new records 
+
+
+
+
+app.get('/compare', (req, res) => {
+    const poolOld = getPoolOld()
+    poolOld.query('SELECT * FROM accounts', (errOld, oldData) => {
+        if (errOld) {
+            console.log(errOld)
+            res.status(400).send(errOld)
+            poolOld.end()
+        } else {
+            const poolNew = getPoolNew()
+            poolNew.query('SELECT * FROM accounts', (errNew, newData) => {
+                if (errNew) {
+                    console.log(errNew)
+                    res.status(400).send(errNew)
+                    poolNew.end()
+                    poolOld.end()
+                }else {
+                    let corruptedObjs = []
+                const compareObj = (newObj, oldObj) => {
+                       if (newObj.name !== oldObj.name || newObj.email !== oldObj.email || Object.keys(newObj).length !== 4 ) {
+                        objCorrupted.push(newObj)
+        }
+                }
+            // todo - find:
+            // 1. Missing data 
+            // 2. corrupted data 
+            // 3. new records 
+
+            // corruptedObjs: 6120
+            newData.rows.forEach(el => {
+                const found = oldData.rows.filter(d => d.id === el.id)
+                if (found.length > 0) {
+                    compareObj(el, found[0])
+                }
+            })
+            res.status(200).send({corrupted: corruptedObjs.length, corruptedObjs})
+        }
+            })
+        } 
+    })
+})
 
 
 
