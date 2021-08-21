bec44b710e44fb660eb063055245cdb27836db88
Luke Gartland <lukegartland@gmail.com>
2021-08-20 20:05:06 -1000

    refactored generateReport to forEach splicing maching records from new data

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,7 @@
 import express from 'express';
 
-// controllers
 import allNewData from './controllers/allNewData.js';
 import allOldData from './controllers/allOldData.js';
-import compareData from './controllers/compareData.js';
-import generateReport from './utils/generateReport.js';
-
-import getNewData from './getData/getNewData.js';
-import getOldData from './getData/getOldData.js';
-
 import report from './controllers/report.js';
 
 const app = express();
@@ -21,7 +14,6 @@ app.get('/', (req, res) => {
 app.get('/old', allOldData);
 app.get('/new', allNewData);
 app.get('/report', report);
-app.get('/compare', compareData);
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
