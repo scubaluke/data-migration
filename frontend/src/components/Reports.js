267f7bddb719ec0c90926aba449c9625dfcba930
Luke Gartland <lukegartland@gmail.com>
2021-08-26 17:35:50 -1000

    fixed typo in Report import removed extra /

--- frontend/src/components/Reports.js
+++ frontend/src/components/Reports.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react'
 import axios from 'axios'
-import Report from './/Report'
+import Report from './Report'
 import Loader from './Loader'
 
 function Reports() {
